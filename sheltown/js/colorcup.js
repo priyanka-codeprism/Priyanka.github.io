ae179526dab04060282283cf1bf7f9e613b012d2
PriyankaUI <66872012+PriyankaUI@users.noreply.github.com>
2020-11-27 12:31:10 -0400

    added

---
+++ sheltown/js/colorcup.js
@@ -0,0 +1,5 @@
+/*var select = document.getElementById("mySelected");
+var bgcolor = document.getElementById('s1').value;
+select.onchange = function () {
+  select.className = this.options[this.selectedIndex].className;
+};*/
